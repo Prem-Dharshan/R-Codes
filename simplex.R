1982ca64bfce7b96a8e56af3eeb190fd3bf10d1a
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-04-07 01:15:43 +0530

    Create simplex

--- simplex.R
+++ simplex.R
@@ -19,10 +19,3 @@ optimal_objective_value <- lp_result$objval
 # Print the optimal solution and the optimal objective function value
 cat("\n\nOptimal Solution: (", optimal_solution[1], ", ", optimal_solution[2], ")\n")
 cat("Optimal Objective Function Value: ", optimal_objective_value, "\n")
-
-plot(optimal_solution, type = "p", pch = 19, col = "red", main = "Linear Programming Problem", xlab = "x", ylab = "y")
-abline(0, 1, col = "blue", lty = 2) # Constraint 1: 6x + 4y <= 24
-abline(0.5, 1, col = "green", lty = 2) # Constraint 2: x + 2y <= 6
-abline(1, -1, col = "purple", lty = 2) # Constraint 3: -x + y <= 1
-abline(0, 1, col = "orange", lty = 2) # Constraint 4: y <= 2
-legend("topright", legend = "Optimal Solution", pch = 19, col = "red")
