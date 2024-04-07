ee12a274463911126cd3f5c81db706e2c05f2026
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-04-07 13:24:08 +0530

    use gmoip fr graphical

--- graphical.R
+++ graphical.R
@@ -1,43 +1,20 @@
-# Define constraints and feasible region
-cons.1 <- function(x) 6 - x  
-cons.2 <- function(x) 5 - 0.5*x
-feasible_region <- data.frame(x = c(0, 4, 4, 2, 0), y = c(0, 0, 2, 4, 5))
+library(gMOIP)
 
-# Define objective function
-objective_function <- function(x) -0.8*x + 2
+A <- matrix(c(6,4,1,2,-1,1,0,1), ncol = 2, byrow = TRUE)
+b <- c(24,6,1,2)
+obj <- c(5,4)  # coefficients c
 
-# Import ggplot2 package
-library(ggplot2)
+plotPolytope(
+  A,
+  b,
+  obj,
+  type = rep("c", ncol(A)),
+  crit = "max",
+  faces = rep("c", ncol(A)),
+  plotFaces = TRUE,
+  plotFeasible = TRUE,
+  plotOptimum = TRUE,
+  labels = "coord",
+  argsFaces = list(argsGeom_polygon = list(fill = 'blue'))
+)
 
-# Build plot
-p <- ggplot() +
-  
-  # Add constraints lines
-  stat_function(fun = cons.1, aes(color = "Constraint 1")) +
-  stat_function(fun = cons.2, aes(color = "Constraint 2")) +
-  
-  # Add feasible region
-  geom_polygon(data = feasible_region, aes(x = x, y = y), fill = "#dddddd", color = "black") +
-  
-  # Define labels
-  labs(title = "Optimization Problem",
-       subtitle = "Graphical Method",
-       x = "x1",
-       y = "x2") +
-  
-  # Set axes limits
-  xlim(0, 10) +
-  ylim(0, 10) +
-  
-  # Add black and white theme
-  theme_bw()
-
-# Add objective function to plot
-p <- p + stat_function(fun = objective_function, aes(color = "Objective Function"), linetype = "dashed")
-
-# Highlight optimum solution in plot
-optimal_solution <- c(4, 2)
-p <- p + geom_point(aes(x = optimal_solution[1], y = optimal_solution[2]), color = "red", size = 4)
-
-# Print plot
-print(p)
