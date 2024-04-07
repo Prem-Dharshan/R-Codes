c8c9f95f5274a83be3e2e22b24452ec20f4a868f
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-04-07 14:24:53 +0530

    Simplify graphical

--- graphical.R
+++ graphical.R
@@ -1,16 +1,16 @@
 library(gMOIP)
 
-A <- matrix(c(6,4,1,2,-1,1,0,1), ncol = 2, byrow = TRUE)
-b <- c(24,6,1,2)
-obj <- c(5,4)  # coefficients c
+constraint_coefficients <- matrix(c(6,4,1,2,-1,1,0,1), ncol = 2, byrow = TRUE)
+rhs <- c(24,6,1,2)
+obj <- c(5,4) 
 
 plotPolytope(
-  A,
-  b,
+  constraint_coefficients,
+  rhs,
   obj,
-  type = rep("c", ncol(A)),
+  type = rep("c", ncol(constraint_coefficients)),
   crit = "max",
-  faces = rep("c", ncol(A)),
+  faces = rep("c", ncol(constraint_coefficients)),
   plotFaces = TRUE,
   plotFeasible = TRUE,
   plotOptimum = TRUE,
